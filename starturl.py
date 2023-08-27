4f72961b49b30a95fc0ac61332e36950eee1c8a5
aravindan <aravindan.acct@gmail.com>
2023-08-27 18:42:30 +0530

    app changes to provide startup configuration

--- starturl.py
+++ starturl.py
@@ -1,7 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template, request, redirect
-
+import os
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -9,15 +9,19 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/starturl')
+@app.route('/settings/starturl')
 # ‘/starturl’ URL is bound with provisioning() function.
 def view_form():
     return render_template('starturl.html')
 
-@app.route('/target', methods=['GET','POST'])
+@app.route('/settings/target', methods=['GET','POST'])
 def provisioning():
 	if request.method == 'POST':
-		target = request.args['target']
+		frontendip = request.form.get('FrontendIP')
+		backendip = request.form.get('BackendIP')
+		os.environ['apiserver'] = backendip
+		os.environ['publicip'] = frontendip
+		print(os.environ.get('apiserver'))
 	return 'Petstore Web App Configured Successfully'
 
 # main driver function
