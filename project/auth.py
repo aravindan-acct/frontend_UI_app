9a7bc35006b29cacfa25fe12511938ca5ff328ab
root <root@appui.bzinpwwjsonenkpy2xlf1hi4rg.bx.internal.cloudapp.net>
2023-09-12 07:39:27 +0000

    input validation for signup

--- project/auth.py
+++ project/auth.py
@@ -5,6 +5,7 @@ from . import db, backend_url, headers
 from flask_login import login_user, logout_user, login_required, current_user
 import requests
 import json
+from input_validator import Validator
 from . import logger
 
 auth = Blueprint('auth', __name__)
@@ -50,9 +51,21 @@ def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
     username = request.form.get('username')
+    if Validator.check_string(str(username)):
+        pass
+    else:
+        return ('Invalid input', 404)
     password = request.form.get('password')
     firstName = request.form.get('firstName')
+    if Validator.check_string(str(firstName)):
+        pass
+    else:
+        return ('Invalid input', 404)
     lastName = request.form.get('lastName')
+    if Validator.check_string(str(lastName)):
+        pass
+    else:
+        return ('Invalid input', 404)
     phone = request.form.get('phone')
 
     #user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
